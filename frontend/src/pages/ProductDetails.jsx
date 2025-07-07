f065b9dc01efd92f2452b8e9fe1d6ae12d48eb95
Kamta Prasad Shahu <130173094+kamtaprasad23@users.noreply.github.com>
2025-07-08 02:38:12 +0530

    Update ProductDetails.jsx

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -8,7 +8,7 @@ const ProductDetail = () => {
 
   const fetchProduct = async () => {
     try {
-      const { data } = await axios.get(`https://e-commerce-website-2ksc.onrender.com/getproduct/${id}`);
+      const { data } = await axios.get(`https://e-commerce-website-backend-oq8z.onrender.com/getproduct/${id}`);
       setProduct(data);
     } catch (err) {
       console.log("Fetch error", err);
