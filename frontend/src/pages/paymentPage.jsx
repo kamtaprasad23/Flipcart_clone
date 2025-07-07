7138512b6423652bf56b25a9fcccf761004446af
Kamta Prasad Shahu <130173094+kamtaprasad23@users.noreply.github.com>
2025-07-08 02:42:21 +0530

    Update paymentPage.jsx

--- frontend/src/pages/paymentPage.jsx
+++ frontend/src/pages/paymentPage.jsx
@@ -7,14 +7,14 @@ const PaymentPage = () => {
   const handlePayment = async () => {
     try {
       // Create order on backend
-      const { data } = await axios.post("https://e-commerce-website-2ksc.onrender.com/payment", {
+      const { data } = await axios.post("https://e-commerce-website-backend-oq8z.onrender.com/payment", {
         amount: amount || 100, // fallback ₹100
       });
 
       const { id: order_id, currency, amount: order_amount } = data.order;
 
       // Get Razorpay key from backend
-      const { data: keyData } = await axios.get("https://e-commerce-website-2ksc.onrender.com/getkey");
+      const { data: keyData } = await axios.get("https://e-commerce-website-backend-oq8z.onrender.com/getkey");
 
       // Razorpay options
       const options = {
